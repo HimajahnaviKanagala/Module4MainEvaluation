d299982e90e41d8026715123241ef39d373031ca
HimajahnaviKanagala <himajahnavikanagala1416@gmail.com>
2026-01-31 21:00:41 +0530

    commit changes

--- Fleet_management_system/controllers/trip.controller.js
+++ Fleet_management_system/controllers/trip.controller.js
@@ -11,10 +11,35 @@ export const createTrip= async(req, res)=>{
     if(!vehicle.isAvailable)
         return res.status(400).json({message:"Vehicle not available"});
 
-    if(paasengers>vehicle.allowed_passengers)
+    if(passengers>vehicle.allowed_passengers)
         return res.status(400).json({message:"Passener limit exceeded!"});
     const tripCost=distance_km* vehicle.rate_per_km;
+
+    await supabase.from("vechicles".update({isAvailable:false})).eq("id", vehicle_id);
+
+    await supabase.from("trips").insert([{
+        ...req.body,
+        customer_id:req.user.id,
+        tripCost;
+    }]);
+    res.status(201).json({message:"Trip created!"});
 };
+
+export const updateTrip=async(req, res)=>{
+    await supabase.from("trips")
+    .update(req.body)
+    .eq("id", req.params.tripId);
+    res.json({message:"Trip updated"});
+};
+
+export const getTrip=async(req, res)=>{
+    const {data}= await supabase.from("trips")
+    .select("*")
+    .eq("id", req.params.tripId)
+    .single();
+
+    res.json(data);
+}
 
 export const deleteTrip= async(req, res)=>{
     await supabase.from("trips").delete().eq("id", req.params.tripId);
--- Fleet_management_system/routes/trip.routes.js
+++ Fleet_management_system/routes/trip.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createTrip, deleteTrip } from "../controllers/trip.controller.js";
+import { createTrip, deleteTrip, updateTrip, getTrip} from "../controllers/trip.controller.js";
 const router=express.Router();
 
 router.post("/create", createTrip);
 router.delete("/delete/:tripId", deleteTrip);
+router.patch("/update/:tripId", updateTrip);
+router.get("/:tripId", getTrip);
 
 export default router;
